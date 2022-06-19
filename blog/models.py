e22acc035996a1d87afb5a3c31561a09184fb720
Oreoluwa Anibaba <oreoluwaanibaba@gmail.com>
2022-06-19 09:33:45 +0100

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  created_date = models.DateTimeField(auto_now_add=True)
+  published_date = models.DateTimeField(auto_now_add=True)
+  
+
+  
+
+
+
+# Create your models here.
